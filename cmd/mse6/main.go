63259db6b45d85359360b63779189c5a03df4170
Simon Mittag <simonmittag@gmail.com>
2020-05-05 13:47:22 +1000

    flags

--- cmd/mse6/main.go
+++ cmd/mse6/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"flag"
 	"github.com/simonmittag/mse6"
-	_ "flag"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "the http port")
 	waitSecs := flag.Int("wait", 3, "wait time for server to respond in seconds")
+	flag.Parse()
 	mse6.Bootstrap(*port, float64(*waitSecs))
 }
