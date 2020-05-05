dd90f155ded669a2bd898fac3fc9eb9a553fc57f
Simon Mittag <simonmittag@gmail.com>
2020-05-05 13:38:41 +1000

    cli

--- cmd/mse6/main.go
+++ cmd/mse6/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"github.com/simonmittag/mse6"
+	_ "flag"
 )
 
 func main() {
-	mse6.Bootstrap(8080, 3)
+	port := flag.Int("port", 8080, "the http port")
+	waitSecs := flag.Int("wait", 3, "wait time for server to respond in seconds")
+	mse6.Bootstrap(*port, float64(*waitSecs))
 }
