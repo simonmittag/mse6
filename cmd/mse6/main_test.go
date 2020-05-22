b5d324024ededc8e5d040322f71cd7e84df79a30
Simon Mittag <simonmittag@gmail.com>
2020-05-22 10:19:06 +1000

    more tests to work out init functions

--- cmd/mse6/main_test.go
+++ cmd/mse6/main_test.go
@@ -35,3 +35,16 @@ func TestParsePrefix(t *testing.T) {
 		t.Errorf("prefix parse error, want %v, got: %v", want, got)
 	}
 }
+
+//tests below only need to execute to work out
+func TestPrintVersion(t *testing.T) {
+	printVersion()
+}
+
+func TestPrintSelfTest(t *testing.T) {
+	printSelftest(61234)
+}
+
+func TestInitLogger(t *testing.T) {
+	initLogger()
+}
